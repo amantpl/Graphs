c804b76452fbcdb94213fa3c3723acd6611d2cde
Aman Thapliyal <kira@Amans-MacBook-Air.local>
2024-12-09 11:17:26 +0530

    Added dijkstras algo using min heap

---
+++ dijkstrasalgo.cpp
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User Function Template
+class Solution {
+  public:
+    // Function to find the shortest distance of all the vertices
+    // from the source vertex src.
+    vector<int> dijkstra(vector<vector<pair<int, int>>> &adj, int src) {
+        // Code here
+        priority_queue<pair<int,int>> q;
+        int n=adj.size();
+        vector<int> dist(n,INT_MAX);
+        
+        q.push({0,src});
+        dist[src]=0;
+
+        while(!q.empty()){
+            auto it=q.top();
+            int distance=it.first;
+            int node=it.second;
+            
+            q.pop();
+            
+            for(auto it1:adj[node]){
+                int dis_from_node=it1.second;
+                int node_connected=it1.first;
+                int dis_from_src=distance+dis_from_node;
+                
+                if(dis_from_src<dist[node_connected]){
+                    dist[node_connected]=dis_from_src;
+                    q.push({dis_from_src,node_connected});
+                }
+                
+            
+            }
+        }
+        return dist;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<pair<int, int>>> adj(V);
+        int i = 0;
+        while (i++ < E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            pair<int, int> t1 = {v, w}, t2 = {u, w};
+            adj[u].push_back(t1);
+            adj[v].push_back(t2);
+        }
+        int src;
+        cin >> src;
+        cin.ignore();
+
+        Solution obj;
+        vector<int> res = obj.dijkstra(adj, src);
+
+        for (int i = 0; i < V; i++)
+            cout << res[i] << " ";
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
