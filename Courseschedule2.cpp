5bc526180bc32e523b56e7e9be2e2e2bacd54271
Aman Thapliyal <kira@Amans-MacBook-Air.local>
2024-12-19 17:12:49 +0530

    Added course schedule 2 using bfs

---
+++ Courseschedule2.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indegree(numCourses,0);
+        vector<vector<int>> adj(numCourses);
+
+        for(auto it:prerequisites){
+            adj[it[0]].push_back(it[1]);
+        }
+
+        for(int i=0;i<numCourses;i++){
+            for(int j=0;j<adj[i].size();j++){
+                indegree[adj[i][j]]++;
+            }
+        }
+
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+
+        vector<int> ans;
+
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+
+            ans.push_back(node);
+
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(ans.size()!=numCourses){
+            ans.clear();
+            return ans;
+        }
+
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
